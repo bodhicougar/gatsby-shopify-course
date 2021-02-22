7e57ab9f2c8bf7af18b03d4be570035862fe5cb2
bodhicougar <info@blackboxmd.fyi>
2021-02-22 09:57:40 +0100

    Alle Änderungen in Deutsch

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,8 +3,8 @@ import { Layout, SEO } from 'components';
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
+    <SEO title="404: Nicht gefunden!" />
+    <h1>NICHT GEFUNDEN</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 );
